4556ff1a18638ddd2d40d059a996a9fb3f5cfdfb
Lelan2610 <lelan26106416@gmail.com>
2025-05-07 17:57:00 +0700

    Create nutrition.py

---
+++ utils/nutrition.py
@@ -0,0 +1,6 @@
+def calculate_tdee(weight, height, age, gender, activity_level):
+    if gender == 'male':
+        bmr = 10 * weight + 6.25 * height - 5 * age + 5
+    else:
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161
+    return bmr * activity_level
